257b6a53cb8124b489cf67fa12f703ed4a7b732d
Olaoluwa Osuntokun <laolu32@gmail.com>
2020-02-07 21:08:00 -0800

    venue/matching: add new UniformPriceCallMarket impl of BatchAuctioneer
    In this commit, we introduce the UniformPriceCallMarket. This is the
    primary struct that the auctioneer will interact with in order to clear
    markets. Orders are to be added to the in-memory cache of the market as
    they're submitted, then finally we attempt a clear with the MaybeClear
    method once the time period is up.

---
+++ venue/matching/matchmaker.go
@@ -0,0 +1,252 @@
+package matching
+
+import (
+	"container/list"
+
+	orderT "github.com/lightninglabs/agora/client/order"
+	"github.com/lightninglabs/agora/order"
+)
+
+// BatchID is a 32-byte identifier that uniquely identifies this batch. This ID
+// will be used later for account key derivation when constructing the batch
+// execution transaction.
+type BatchID [32]byte
+
+// UniformPriceCallMarket is a discrete-batch auction that clears all orders in
+// a batch according to a selected uniform clearing price. This struct will be
+// used by the main auctioneer system to clear batches ever period T, or as
+// frequently as is needed.
+//
+// NOTE: This is an implementation of the BatchAuctioneer interface.
+type UniformPriceCallMarket struct {
+	// bids is a linked list of all bids.
+	bids *list.List
+
+	// bidIndex is an index into the above linked list so we can easily
+	// remove bids that are cancelled or matched.
+	bidIndex map[orderT.Nonce]*list.Element
+
+	// asks is a linked list of all asks.
+	asks *list.List
+
+	// askIndex is an index into the above linked list so we can easily
+	// remove asks that are cancelled or matched.
+	askIndex map[orderT.Nonce]*list.Element
+
+	// priceClearer is the main instance that the call market will used to
+	// arrive at the uniform clearing price.
+	priceClearer PriceClearer
+
+	// feeSchedule is the current fee schedule of the auctioneer. This will
+	// be used to determine how much to charge traders in venue and
+	// execution fees.
+	feeSchedule FeeSchedule
+}
+
+// NewUniformPriceCallMarket returns a new instance of the
+// UniformPriceCallMarket struct given the price clearer and fee schedule for
+// this current batch epoch.
+func NewUniformPriceCallMarket(priceClearer PriceClearer,
+	feeSchedule FeeSchedule) *UniformPriceCallMarket {
+
+	u := &UniformPriceCallMarket{
+		priceClearer: priceClearer,
+		feeSchedule:  feeSchedule,
+	}
+	u.resetOrderState()
+
+	return u
+
+}
+
+// resetOrderState resets the order state to blank.
+func (u *UniformPriceCallMarket) resetOrderState() {
+	u.bids = list.New()
+	u.bidIndex = make(map[orderT.Nonce]*list.Element)
+
+	u.asks = list.New()
+	u.askIndex = make(map[orderT.Nonce]*list.Element)
+}
+
+// MaybeClear attempts to clear a batch given a BatchID. Note that it's
+// possible no match is possible, in which case an error will be
+// returned.
+//
+// NOTE: This method is a part of the BatchAuctioneer interface.
+func (u *UniformPriceCallMarket) MaybeClear(BatchID) (*OrderBatch, error) {
+	// At this point we know we have a set of orders, so we'll create the
+	// match maker for usage below.
+	matchMaker := NewMultiUnitMatchMaker()
+
+	// At this point, there's may be at least a single order that we can
+	// execute, so we'll attempt to match the entire pending batch.
+	//
+	// First we'll obtain slices pointing to the backing list so the match
+	// maker can examine all the entries easily.
+	bids := make([]*order.Bid, 0, u.bids.Len())
+	for bid := u.bids.Front(); bid != nil; bid = bid.Next() {
+		b := bid.Value.(order.Bid)
+		bids = append(bids, &b)
+	}
+	asks := make([]*order.Ask, 0, u.asks.Len())
+	for ask := u.asks.Front(); ask != nil; ask = ask.Next() {
+		a := ask.Value.(order.Ask)
+		asks = append(asks, &a)
+	}
+
+	// With our bids/asks converted to the proper container type, we'll
+	// attempt to perform match making for the entire batch to make the
+	// market for this next batch.
+	matchSet, err := matchMaker.MatchBatch(bids, asks)
+	if err != nil {
+		// TODO(roasbeef): wrapped errors thru the entire codebase?
+		return nil, err
+	}
+
+	// If we have no matched orders at all, then we can exit early here, as
+	// there's no market to make.
+	if len(matchSet.MatchedOrders) == 0 {
+		return nil, ErrNoMarketPossible
+	}
+
+	// Now that we know all the orders to be executed for this batch (and
+	// the stragglers), we'll compute the uniform clearing price with the
+	// current price clearer instance.
+	clearingPrice, err := u.priceClearer.ExtractClearingPrice(matchSet)
+	if err != nil {
+		return nil, err
+	}
+
+	// Now that we know which orders will execute in this batch, we'll
+	// update our in-memory set of orders so we can properly make a market
+	// for the next batch.
+	//
+	// TODO(roasbeef): remove orders instead?
+	u.resetOrderState()
+	if err := u.ConsiderBids(matchSet.UnmatchedBids...); err != nil {
+		return nil, err
+	}
+	if err := u.ConsiderAsks(matchSet.UnmatchedAsks...); err != nil {
+		return nil, err
+	}
+
+	// Next, we'll also add any orders that weren't fully consumed we'll
+	// also re-add those to the current active order set.
+	for _, order := range matchSet.MatchedOrders {
+		bid := order.Details.Bid
+		if bid.UnitsUnfulfilled != 0 {
+			if err := u.ConsiderBids(bid); err != nil {
+				return nil, err
+			}
+		}
+
+		ask := order.Details.Ask
+		if ask.UnitsUnfulfilled != 0 {
+			if err := u.ConsiderAsks(ask); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// As a final step, we'll compute the diff for each trader's account.
+	// With this final piece of information, the caller will be able to
+	// easily update all the order/account state in a single atomic
+	// transaction.
+	feeReport := NewTradingFeeReport(
+		matchSet.MatchedOrders, u.feeSchedule, clearingPrice,
+	)
+
+	return &OrderBatch{
+		Orders:        matchSet.MatchedOrders,
+		FeeReport:     feeReport,
+		ClearingPrice: clearingPrice,
+	}, nil
+}
+
+// ConsiderBid adds a set of bids to the staging arena for match making. Only
+// once a bid has been considered will it be eligible to be included in an
+// OrderBatch.
+//
+// NOTE: This is a part of the BatchAuctioneer interface.
+func (u *UniformPriceCallMarket) ConsiderBids(bids ...*order.Bid) error {
+	// We'll add all the bids in a single batch, while keeping our pointer
+	// to the "best" (highest) bid in the batch up to date.
+	for _, bid := range bids {
+		bid := bid
+
+		// If the order is already in the set, then we'll skip it to
+		// avoid adding it twice.
+		orderNonce := bid.Nonce()
+		if _, ok := u.bidIndex[orderNonce]; ok {
+			continue
+		}
+
+		bidElement := u.bids.PushBack(*bid)
+		u.bidIndex[orderNonce] = bidElement
+	}
+
+	return nil
+}
+
+// ForgetBid removes a set of bids from the staging area.
+//
+// NOTE: This is a part of the BatchAuctioneer interface.
+func (u *UniformPriceCallMarket) ForgetBids(bids ...orderT.Nonce) error {
+	for _, bidNonce := range bids {
+		bidElement, ok := u.bidIndex[bidNonce]
+		if !ok {
+			continue
+		}
+
+		u.bids.Remove(bidElement)
+		delete(u.bidIndex, bidNonce)
+	}
+
+	return nil
+}
+
+// ConsiderAsk adds a set of asks to the staging arena for match making. Only
+// once an ask has been considered will it be eligible to be included in an
+// OrderBatch.
+//
+// NOTE: This is a part of the BatchAuctioneer interface.
+func (u *UniformPriceCallMarket) ConsiderAsks(asks ...*order.Ask) error {
+	// We'll add all the asks in a single batch, while keeping our pointer
+	// to the "best" (lowest) ask in the batch up to date.
+	for _, ask := range asks {
+		ask := ask
+
+		// If the order is already in the set, then we'll skip it to
+		// avoid adding it twice.
+		orderNonce := ask.Nonce()
+		if _, ok := u.askIndex[orderNonce]; ok {
+			continue
+		}
+
+		askElement := u.asks.PushBack(*ask)
+		u.askIndex[orderNonce] = askElement
+	}
+
+	return nil
+}
+
+// ForgetAsk removes a set of asks from the staging area.
+//
+// NOTE: This is a part of the BatchAuctioneer interface.
+func (u *UniformPriceCallMarket) ForgetAsks(asks ...orderT.Nonce) error {
+	for _, askNonce := range asks {
+		askElement, ok := u.askIndex[askNonce]
+		if !ok {
+			continue
+		}
+
+		u.asks.Remove(askElement)
+		delete(u.askIndex, askNonce)
+	}
+
+	return nil
+}
+
+// A compile-time assertion to ensure that the UniformPriceCallMarket meets the
+// BatchAuctioneer interface.
+var _ BatchAuctioneer = (*UniformPriceCallMarket)(nil)
---
+++ venue/matching/matchmaker_test.go
@@ -0,0 +1,349 @@
+package matching
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"testing"
+	"testing/quick"
+	"time"
+
+	orderT "github.com/lightninglabs/agora/client/order"
+)
+
+// TestCallMarketConsiderForgetOrders tests that we're able to properly add and
+// remove orders from the uniform price call market.
+func TestCallMarketConsiderForgetOrders(t *testing.T) {
+	t.Parallel()
+
+	// In this scenario, we test that given a set of bids (clamping to
+	// ensure we don't have too long a runtime. We're able to add all the
+	// bids, then remove all of them resulting in an empty set of bids. The
+	// bids index should also be blank at this point.
+	scenario := func(orders orderSet) bool {
+		callMarket := NewUniformPriceCallMarket(
+			&LastAcceptedBid{}, &mockFeeSchedule{1, 100000},
+		)
+
+		if err := callMarket.ConsiderBids(orders.Bids...); err != nil {
+			t.Fatalf("unable to add bids")
+			return false
+		}
+
+		// Trying to remove a bid that doesn't exist should have no
+		// effect on the bids inserted.
+		var blankNonce orderT.Nonce
+		if err := callMarket.ForgetBids(blankNonce); err != nil {
+			t.Fatalf("unable to forget asks")
+			return false
+		}
+
+		// We'll add the set of bids again to ensure no bids are double
+		// added.
+		if err := callMarket.ConsiderBids(orders.Bids...); err != nil {
+			t.Fatalf("unable to forget bids")
+			return false
+		}
+
+		// At this point, ever bid that we added should be found in the
+		// bidIndex.
+		for _, bid := range orders.Bids {
+			if _, ok := callMarket.bidIndex[bid.Nonce()]; !ok {
+				t.Fatalf("bid not found!")
+			}
+		}
+
+		// Next we'll add our set of adds and repeat the same assertion
+		// (adding twice to ensure they aren't double counted).
+		if err := callMarket.ConsiderAsks(orders.Asks...); err != nil {
+			t.Logf("unable to add bids")
+			return false
+		}
+		if err := callMarket.ConsiderAsks(orders.Asks...); err != nil {
+			t.Logf("unable to add asks")
+			return false
+		}
+
+		// Trying to remove a bid that doesn't exist should have no
+		// effect on the bids inserted.
+		if err := callMarket.ForgetAsks(blankNonce); err != nil {
+			t.Logf("unable to forget asks")
+			return false
+		}
+
+		if err := callMarket.ForgetBids(blankNonce); err != nil {
+			t.Logf("unable to forget asks")
+			return false
+		}
+		for _, ask := range orders.Asks {
+			if _, ok := callMarket.askIndex[ask.Nonce()]; !ok {
+				t.Fatalf("ask not found!")
+			}
+		}
+
+		// The total number of bids and asks should match the amount we
+		// inserted above.
+		switch {
+		case len(callMarket.bidIndex) != len(orders.Bids):
+
+			t.Fatalf("wrong number of bids: got %v, expected %v",
+				callMarket.bids.Len(), len(orders.Bids))
+			return false
+
+		case len(callMarket.askIndex) != len(orders.Asks):
+
+			t.Fatalf("wrong number of asks: got %v, expected %v",
+				callMarket.asks.Len(), len(orders.Asks))
+			return false
+		}
+
+		// Now if we forget all the bids and asks, both the internal
+		// list as well as the index should be empty.
+		for _, bid := range orders.Bids {
+			if err := callMarket.ForgetBids(bid.Nonce()); err != nil {
+				t.Logf("unable to forget bids")
+				return false
+			}
+		}
+		for _, ask := range orders.Asks {
+			if err := callMarket.ForgetAsks(ask.Nonce()); err != nil {
+				t.Logf("unable to forget asks")
+				return false
+			}
+		}
+
+		switch {
+		case len(callMarket.bidIndex) != 0:
+			t.Logf("bid index not size zero, instead: %v",
+				len(callMarket.bidIndex))
+			return false
+
+		case len(callMarket.askIndex) != 0:
+			t.Logf("ask index not size zero, instead: %v",
+				len(callMarket.askIndex))
+			return false
+
+		case callMarket.bids.Len() != 0:
+			t.Logf("bid list not size zero, instead: %v",
+				callMarket.bids.Len())
+			return false
+
+		case callMarket.asks.Len() != 0:
+			t.Logf("ask list not size zero, instead: %v",
+				callMarket.asks.Len())
+			return false
+		}
+
+		return true
+	}
+	quickCfg := quick.Config{
+		Values: func(v []reflect.Value, r *rand.Rand) {
+			// When generating the random set below, we'll cap the
+			// number of orders on both sides to ensure the test
+			// completes in a timely manner.
+			randOrderSet := genRandOrderSet(r, 1000)
+
+			v[0] = reflect.ValueOf(randOrderSet)
+		},
+	}
+	if err := quick.Check(scenario, &quickCfg); err != nil {
+		t.Fatalf("call market order scenario: %v", err)
+	}
+}
+
+// TestMaybeClearNoOrders tests that if no orders exist, and a match is
+// attempted, we get back ErrNoMarketPossible.
+func TestMaybeClearNoOrders(t *testing.T) {
+	t.Parallel()
+
+	callMarket := NewUniformPriceCallMarket(
+		&LastAcceptedBid{}, &mockFeeSchedule{1, 100000},
+	)
+
+	_, err := callMarket.MaybeClear(BatchID{})
+	if err != ErrNoMarketPossible {
+		t.Fatalf("expected ErrNoMarketPossible, instead got: %v", err)
+	}
+}
+
+// TestMaybeClearNoClearPossible tests that if there isn't a clear possible,
+// then we should receive an error and no matches are produced.
+func TestMaybeClearNoClearPossible(t *testing.T) {
+	t.Parallel()
+
+	// First, we'll generate a bid and ask that can't match as the rate
+	// they request is incompatible.
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	bid := genRandBid(r, staticRateGen(100))
+	ask := genRandAsk(r, staticRateGen(1000))
+
+	// Next, we'll create our call market, and add these two incompatible
+	// orders.
+	callMarket := NewUniformPriceCallMarket(
+		&LastAcceptedBid{}, &mockFeeSchedule{1, 100000},
+	)
+	if err := callMarket.ConsiderBids(bid); err != nil {
+		t.Fatalf("unable to add bids")
+	}
+	if err := callMarket.ConsiderAsks(ask); err != nil {
+		t.Fatalf("unable to add asks")
+	}
+
+	// If we attempt to make a market, we should get the
+	// ErrNoMarketPossible error.
+	_, err := callMarket.MaybeClear(BatchID{})
+	if err != ErrNoMarketPossible {
+		t.Fatalf("expected ErrNoMarketPossible, got: %v", err)
+	}
+
+	// The bid and ask that we added above, should also still remain in
+	// place within the call market.
+	if _, ok := callMarket.bidIndex[bid.Nonce()]; !ok {
+		t.Fatalf("bid not found in market")
+	}
+	if _, ok := callMarket.askIndex[ask.Nonce()]; !ok {
+		t.Fatalf("ask not found in market")
+	}
+}
+
+// TestMaybeClearClearingPriceConsistency tests that once a market has been
+// cleared, all internal state is consistent with the outcome of the matching
+// event.
+func TestMaybeClearClearingPriceConsistency(t *testing.T) {
+	t.Parallel()
+
+	n, y := 0, 0
+	scenario := func(orders orderSet) bool {
+		// We'll start with making a new call market,
+		callMarket := NewUniformPriceCallMarket(
+			&LastAcceptedBid{}, &mockFeeSchedule{1, 100000},
+		)
+		if err := callMarket.ConsiderBids(orders.Bids...); err != nil {
+			t.Logf("unable to add bids")
+			return false
+		}
+		if err := callMarket.ConsiderAsks(orders.Asks...); err != nil {
+			t.Logf("unable to add asks")
+			return false
+		}
+
+		// We'll now attempt to make a market, if no market can be
+		// made, then we'll go to the next scenario.
+		orderBatch, err := callMarket.MaybeClear(BatchID{})
+		if err != nil {
+			fmt.Println("clear error: ", err)
+			n++
+			return true
+		}
+
+		// At this point we know that we have a match.
+		//
+		// First, we'll ensure that the set of internal orders are
+		// consistent.
+		matchedOrders := orderBatch.Orders
+		fullyConsumedOrders := make(map[orderT.Nonce]struct{})
+		for _, matchedOrder := range matchedOrders {
+			bid := matchedOrder.Details.Ask
+			ask := matchedOrder.Details.Bid
+
+			// If the order set was totally filled, then it
+			// shouldn't be found in current set of active orders.
+			if bid.UnitsUnfulfilled == 0 {
+				if _, ok := callMarket.bidIndex[bid.Nonce()]; ok {
+					t.Logf("bid found in active orders " +
+						"after total fulfill")
+					return false
+				}
+
+				bidNonce := bid.Nonce()
+				fullyConsumedOrders[bidNonce] = struct{}{}
+			}
+			if ask.UnitsUnfulfilled == 0 {
+				if _, ok := callMarket.askIndex[ask.Nonce()]; ok {
+					t.Logf("ask found in active orders " +
+						"after total fulfill")
+					return false
+				}
+
+				askNonce := ask.Nonce()
+				fullyConsumedOrders[askNonce] = struct{}{}
+			}
+		}
+
+		// Next, any order that wasn't fully consumed, should be found
+		// in the set of active orders.
+		for _, order := range matchedOrders {
+			bid := order.Details.Bid
+			if _, ok := fullyConsumedOrders[bid.Nonce()]; !ok {
+				if _, ok := callMarket.bidIndex[bid.Nonce()]; !ok {
+					t.Logf("partial bid fill not found in " +
+						"active order set")
+					return false
+				}
+			}
+
+			ask := order.Details.Ask
+			if _, ok := fullyConsumedOrders[ask.Nonce()]; !ok {
+				if _, ok := callMarket.askIndex[ask.Nonce()]; !ok {
+					t.Logf("partial ask fill not found in " +
+						"active order set")
+					return false
+				}
+			}
+		}
+
+		// Finally, the final clearing price should be the same as at
+		// least one of the matched pairs. We use this type of
+		// assertion as it makes it generic w.r.t the clearing price
+		// algo used.
+		var clearingPriceFound bool
+		for _, orderMatch := range matchedOrders {
+			quote := orderMatch.Details.Quote
+			if quote.MatchingRate == orderBatch.ClearingPrice {
+				clearingPriceFound = true
+				break
+			}
+		}
+
+		if !clearingPriceFound {
+			t.Logf("clearing price of %v not found in set of "+
+				"matched orders", orderBatch.ClearingPrice)
+			return false
+		}
+
+		y++
+		return true
+	}
+	quickCfg := quick.Config{
+		Values: func(v []reflect.Value, r *rand.Rand) {
+			// For this case, we'll generate a set of random
+			// orders, as we'll get to test both the case where no
+			// orders match, or only a sub-set of the orders match
+			// and the intermediate state needs to be updated.
+			randOrderSet := genRandOrderSet(r, 1000)
+
+			// We'll also supplements this set of orders with an
+			// pair of orders that we know will be totally filled.
+			sameUnitSet := genRandOrderSet(
+				r, 1,
+				staticRateGen(1000),
+				staticUnitGen(1000),
+				staticDurationGen(2),
+			)
+
+			randOrderSet.Bids = append(
+				randOrderSet.Bids, sameUnitSet.Bids...,
+			)
+			randOrderSet.Asks = append(
+				randOrderSet.Asks, sameUnitSet.Asks...,
+			)
+
+			v[0] = reflect.ValueOf(randOrderSet)
+		},
+	}
+	if err := quick.Check(scenario, &quickCfg); err != nil {
+		t.Fatalf("clearing price consistency scenario failed: %v", err)
+	}
+
+	t.Logf("Total number of scenarios run: %v (%v positive, %v negative)", n+y, y, n)
+}
