23d5951c4283a92bdf0a8fe861c16972ea181b5d
Olaoluwa Osuntokun <laolu32@gmail.com>
2020-03-04 19:59:34 -0800

    account: add weight estimation for the auctioneer's output, witness gen

--- account/script.go
+++ account/script.go
@@ -3,7 +3,29 @@ package account
 import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/input"
+	"github.com/lightningnetwork/lnd/keychain"
+)
+
+const (
+	// AuctioneerKeyFamily is the key family that we'll use to derive the
+	// singleton auctioneer key.
+	AuctioneerKeyFamily keychain.KeyFamily = 221
+
+	// AuctioneerWitnessScriptSize: 35 bytes
+	//    - OP_DATA: 1 byte
+	//    - <pubkey: 33 bytes
+	//    - OP_CHECKSIG: 1 byte
+	AuctioneerWitnessScriptSize = 35
+
+	// AuctioneerWitnessSize: 108
+	//    - num_witness_elements: 1 byte
+	//    - sig_len_varint: 1 byte
+	//    - <sig>: 73 bytes
+	//    - witness_script_len_varint: 1 byte
+	//    - witness_script: 35 bytes
+	AuctioneerWitnessSize = 76 + AuctioneerWitnessScriptSize
 )
 
 // AuctioneerWitnessScript is the witness script that the auctioneer will use
@@ -19,6 +41,8 @@ import (
 //
 // The auctioneerBatchKey is derived as:
 //  * auctioneerBatchKey = auctioneerKey + sha256(batchKey || auctioneerKey)
+//
+// The witness for the script is simply: <sig> <witnessScript>
 func AuctioneerWitnessScript(batchKey, auctioneerKey *btcec.PublicKey) ([]byte, error) {
 	auctioneerBatchKey := input.TweakPubKey(auctioneerKey, batchKey)
 
@@ -39,4 +63,17 @@ func AuctioneerAccountScript(batchKey, auctioneerKey *btcec.PublicKey) ([]byte, 
 	}
 
 	return input.WitnessScriptHash(witnessScript)
+}
+
+// AuctioneerWitness constructs a valid witness for spending the auctioneer's
+// master account. This witness is simply: <sig> <witnessScript>
+//
+// NOTE: The sig passed in should not already have a sighash flag applied,
+// we'll attach on here ourselves manually.
+func AuctioneerWitness(witnessScript, sig []byte) wire.TxWitness {
+	witness := make(wire.TxWitness, 2)
+	witness[0] = append(sig, byte(txscript.SigHashAll))
+	witness[1] = witnessScript
+
+	return witness
 }
