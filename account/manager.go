5e67a76399816d9581927b757bd745e9e926e4c6
Olaoluwa Osuntokun <laolu32@gmail.com>
2020-05-18 16:06:34 -0700

    account: raise max account value to 1 BTC

--- account/manager.go
+++ account/manager.go
@@ -33,7 +33,7 @@ const (
 	// extremes for valid account values. The maximum value is based on the
 	// maximum channel size plus some leeway to account for chain fees.
 	minAccountValue btcutil.Amount = 100000
-	maxAccountValue btcutil.Amount = minAccountValue + (1 << 24) - 1
+	maxAccountValue btcutil.Amount = btcutil.SatoshiPerBitcoin
 
 	// minAccountExpiry and maxAccountExpiry represent the thresholds at
 	// both extremes for valid account expirations.
