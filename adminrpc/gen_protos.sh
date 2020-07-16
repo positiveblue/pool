b2633668c9eeb7b90b1a45e8778d93ffc30d2d1f
Oliver Gugger <gugger@gmail.com>
2020-07-16 13:46:22 +0200

    adminrpc: import auctioneer.proto from $GOPATH
    To be able to reuse messages from the auctioneer.proto we import it from
    the $GOPATH. This should always pull in the version we have referenced
    in our go.mod file. We need to add a PR to the client repo if we want to
    change the auctioneer.proto anyway so we might also reference it here as
    well instead of having a separate copy (which only brings problems if
    the target package differs from the actual implementation used).

--- adminrpc/gen_protos.sh
+++ adminrpc/gen_protos.sh
@@ -1,12 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+# Find out what version of llm to use for the imported auctioneer.proto by
+# parsing the go.mod file.
+LAST_LINE=$(grep "lightninglabs/llm" ../go.mod | tail -n 1)
+PKG_PATH=""
+if [[ $LAST_LINE =~ "replace" ]]
+then
+  # There is a replace directive. Use awk to split by spaces then extract field
+  # 4 and 5 and stitch them together with an @ which will resolve into
+  # github.com/lightninglabs/llm@v0.x.x-yyyymmddhhmiss-shortcommit.
+  PKG_PATH=$(echo $LAST_LINE | awk -F " " '{ print $4"@"$5 }')
+else
+  # This is a normal directive, just combine field 1 and 2 with an @.
+  PKG_PATH=$(echo $LAST_LINE | awk -F " " '{ print $1"@"$2 }')
+fi
 
 # Generate the gRPC bindings for all proto files.
 for file in ./*.proto
 do
 	protoc -I/usr/local/include -I. \
-	       -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+	       -I$GOPATH/pkg/mod/$PKG_PATH \
 	       --go_out=plugins=grpc,paths=source_relative:. \
 		${file}
 
