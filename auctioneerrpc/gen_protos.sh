476e8209466487c70bd052016252c0051723d3ab
Oliver Gugger <gugger@gmail.com>
2021-01-25 10:59:56 +0100

    auctioneerrpc: add REST annotations
    To simplify exposing certain statistics in an easy to process format, we
    add REST annotations to some selected RPCs to allow querying those
    statistics in a JSON format.

--- auctioneerrpc/gen_protos.sh
+++ auctioneerrpc/gen_protos.sh
@@ -12,6 +12,19 @@ function generate() {
   done
 }
 
+# generate_gateway compiles the REST gateway stubs for a given file.
+function generate_gateway() {
+    # Generate the REST reverse proxy.
+  protoc -I/usr/local/include -I. -I.. \
+    --grpc-gateway_out=logtostderr=true,paths=source_relative,grpc_api_configuration=rest-annotations.yaml:. \
+    "$1"
+
+  # Finally, generate the swagger file which describes the REST API in detail.
+  protoc -I/usr/local/include -I. -I.. \
+    --swagger_out=logtostderr=true,grpc_api_configuration=rest-annotations.yaml:. \
+    "$1"
+}
+
 # format formats the *.proto files with the clang-format utility.
 function format() {
   find . -name "*.proto" -print0 | xargs -0 clang-format --style=file -i
@@ -27,4 +40,5 @@ popd
 pushd auctioneerrpc
 format
 generate
+generate_gateway auctioneer.proto
 popd
