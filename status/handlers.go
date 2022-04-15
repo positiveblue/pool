868dcdef9f7d0f4f780d846e37925a26c22e30f7
positiveblue <jordi.montes.sanabria@gmail.com>
2022-04-15 11:12:49 -0700

    status: remove update status post endpoint
    This package is used for other applications and the server status port
    may end up exposed which means that being able to change the status is a
    potential security hole.

--- status/handlers.go
+++ status/handlers.go
@@ -2,7 +2,6 @@ package status
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -37,52 +36,23 @@ func (r *Reporter) ReadinessHandler() func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-// StatusHandler supports the GET and POST methods. GET will return information
-// about the current status while POST will update it.
+// StatusHandler returns information about the current status.
 func (r *Reporter) StatusHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		// GET: Return the current status.
 		if req.Method == http.MethodGet {
 			status := r.GetStatus()
 			respBody, err := json.Marshal(status)
 			if err != nil {
+				log.Errorf("unable to marshal status: %v", err)
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			if _, err := w.Write(respBody); err != nil {
-				log.Info("unable to write resp body: %v", err)
+				log.Errorf("unable to write resp body: %v", err)
 			}
-			return
-		}
-
-		// POST: Update the current status.
-		if req.Method == http.MethodPost {
-			var body struct {
-				Status string
-			}
-
-			b, err := ioutil.ReadAll(req.Body)
-			defer req.Body.Close()
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			if err = json.Unmarshal(b, &body); err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			if err := r.SetStatus(body.Status); err != nil {
-				log.Info("unable to set status: %v", err)
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			w.WriteHeader(http.StatusOK)
 			return
 		}
 
