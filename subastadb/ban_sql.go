c1a8cd26a598f4a5ebf1279bc5ea6d457132c579
positiveblue <jordi.montes.sanabria@gmail.com>
2022-05-15 16:28:37 -0700

    subastadb: implement ban methods for SQLStore

--- subastadb/ban_sql.go
+++ subastadb/ban_sql.go
@@ -2,16 +2,41 @@ package subastadb
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/jackc/pgx/v4"
 	"github.com/lightninglabs/subasta/ban"
+	"github.com/lightninglabs/subasta/subastadb/postgres"
 )
 
 // BanAccount creates/updates the ban Info for the given accountKey.
 func (s *SQLStore) BanAccount(ctx context.Context, accountKey *btcec.PublicKey,
 	info *ban.Info) error {
 
-	return ErrNotImplemented
+	traderKey := accountKey.SerializeCompressed()
+	txBody := func(txQueries *postgres.Queries) error {
+		// Make sure that all the other bans are disabled.
+		_, err := txQueries.DisableAccountBan(ctx, traderKey)
+		if err != nil {
+			return err
+		}
+		expiryHeight := int64(info.Height) + int64(info.Duration)
+		params := postgres.CreateAccountBanParams{
+			Disabled:     false,
+			TraderKey:    traderKey,
+			ExpiryHeight: expiryHeight,
+			Duration:     int64(info.Duration),
+		}
+		return txQueries.CreateAccountBan(ctx, params)
+	}
+
+	err := s.ExecTx(ctx, txBody)
+	if err != nil {
+		return fmt.Errorf("unable to ban account(%x): %v", traderKey,
+			err)
+	}
+	return nil
 }
 
 // GetAccountBan returns the ban Info for the given accountKey.
@@ -19,22 +44,79 @@ func (s *SQLStore) BanAccount(ctx context.Context, accountKey *btcec.PublicKey,
 func (s *SQLStore) GetAccountBan(ctx context.Context,
 	accKey *btcec.PublicKey, currentHeight uint32) (*ban.Info, error) {
 
-	return nil, ErrNotImplemented
+	traderKey := accKey.SerializeCompressed()
+	params := postgres.GetAccountBanParams{
+		TraderKey:    traderKey,
+		ExpiryHeight: int64(currentHeight),
+	}
+	row, err := s.queries.GetAccountBan(ctx, params)
+	switch {
+	case err == pgx.ErrNoRows:
+		return nil, nil
+
+	case err != nil:
+		return nil, fmt.Errorf("unable to get account(%x) ban: %v",
+			traderKey, err)
+	}
+
+	return &ban.Info{
+		Height:   uint32(row.ExpiryHeight) - uint32(row.Duration),
+		Duration: uint32(row.Duration),
+	}, nil
 }
 
 // BanNode creates/updates the ban Info for the given nodeKey.
-func (s *SQLStore) BanNode(ctx context.Context, nodeKey *btcec.PublicKey,
+func (s *SQLStore) BanNode(ctx context.Context, nKey *btcec.PublicKey,
 	info *ban.Info) error {
 
-	return ErrNotImplemented
+	nodeKey := nKey.SerializeCompressed()
+	txBody := func(txQueries *postgres.Queries) error {
+		// Make sure that all the other bans are disabled.
+		_, err := txQueries.DisableNodeBan(ctx, nodeKey)
+		if err != nil {
+			return err
+		}
+		expiryHeight := int64(info.Height) + int64(info.Duration)
+		params := postgres.CreateNodeBanParams{
+			Disabled:     false,
+			NodeKey:      nodeKey,
+			ExpiryHeight: expiryHeight,
+			Duration:     int64(info.Duration),
+		}
+		return txQueries.CreateNodeBan(ctx, params)
+	}
+
+	err := s.ExecTx(ctx, txBody)
+	if err != nil {
+		return fmt.Errorf("unable to ban node(%x): %v", nodeKey, err)
+	}
+	return nil
 }
 
 // GetNodeBan returns the ban Info for the given nodeKey.
 // Info will be nil if the node is not currently banned.
 func (s *SQLStore) GetNodeBan(ctx context.Context,
-	nodeKey *btcec.PublicKey, currentHeight uint32) (*ban.Info, error) {
+	nKey *btcec.PublicKey, currentHeight uint32) (*ban.Info, error) {
 
-	return nil, ErrNotImplemented
+	nodeKey := nKey.SerializeCompressed()
+	params := postgres.GetNodeBanParams{
+		NodeKey:      nodeKey,
+		ExpiryHeight: int64(currentHeight),
+	}
+	row, err := s.queries.GetNodeBan(ctx, params)
+	switch {
+	case err == pgx.ErrNoRows:
+		return nil, nil
+
+	case err != nil:
+		return nil, fmt.Errorf("unable to get node(%x) ban: %v",
+			nodeKey, err)
+	}
+
+	return &ban.Info{
+		Height:   uint32(row.ExpiryHeight) - uint32(row.Duration),
+		Duration: uint32(row.Duration),
+	}, nil
 }
 
 // ListBannedAccounts returns a map of all accounts that are currently banned.
@@ -42,7 +124,26 @@ func (s *SQLStore) GetNodeBan(ctx context.Context,
 func (s *SQLStore) ListBannedAccounts(ctx context.Context,
 	currentHeight uint32) (map[[33]byte]*ban.Info, error) {
 
-	return nil, ErrNotImplemented
+	rows, err := s.queries.GetAllAccountBans(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get all account bans: %v",
+			err)
+	}
+
+	bans := make(map[[33]byte]*ban.Info, len(rows))
+	for _, row := range rows {
+		var traderKey [33]byte
+		copy(traderKey[:], row.TraderKey)
+
+		expiry := uint32(row.ExpiryHeight)
+		duration := uint32(row.Duration)
+		bans[traderKey] = &ban.Info{
+			Height:   expiry - duration,
+			Duration: duration,
+		}
+	}
+
+	return bans, nil
 }
 
 // ListBannedNodes returns a map of all nodes that are currently banned.
@@ -50,7 +151,25 @@ func (s *SQLStore) ListBannedAccounts(ctx context.Context,
 func (s *SQLStore) ListBannedNodes(ctx context.Context,
 	currentHeight uint32) (map[[33]byte]*ban.Info, error) {
 
-	return nil, ErrNotImplemented
+	rows, err := s.queries.GetAllNodeBans(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get all node bans: %v", err)
+	}
+
+	bans := make(map[[33]byte]*ban.Info, len(rows))
+	for _, row := range rows {
+		var nodeKey [33]byte
+		copy(nodeKey[:], row.NodeKey)
+
+		expiry := uint32(row.ExpiryHeight)
+		duration := uint32(row.Duration)
+		bans[nodeKey] = &ban.Info{
+			Height:   expiry - duration,
+			Duration: duration,
+		}
+	}
+
+	return bans, nil
 }
 
 // RemoveAccountBan removes the ban information for a given trader's account
@@ -58,13 +177,27 @@ func (s *SQLStore) ListBannedNodes(ctx context.Context,
 func (s *SQLStore) RemoveAccountBan(ctx context.Context,
 	acctKey *btcec.PublicKey) error {
 
-	return ErrNotImplemented
+	// Disable old bans.
+	traderKey := acctKey.SerializeCompressed()
+	_, err := s.queries.DisableAccountBan(ctx, traderKey)
+	if err != nil {
+		return fmt.Errorf("unable to disable account(%x) bans: %v",
+			traderKey, err)
+	}
+	return nil
 }
 
 // RemoveNodeBan removes the ban information for a given trader's node identity
 // key. Returns an error if no ban exists.
 func (s *SQLStore) RemoveNodeBan(ctx context.Context,
-	nodeKey *btcec.PublicKey) error {
+	nKey *btcec.PublicKey) error {
 
-	return ErrNotImplemented
+	// Disable old bans.
+	nodeKey := nKey.SerializeCompressed()
+	_, err := s.queries.DisableNodeBan(ctx, nodeKey)
+	if err != nil {
+		return fmt.Errorf("unable to disable node(%x) bans: %v",
+			nodeKey, err)
+	}
+	return nil
 }
