2af39ea4b73663ee36bd4950c05c26b90d8d7779
positiveblue <jordi.montes.sanabria@gmail.com>
2022-05-15 17:29:38 -0700

    subastadb: implement node ratings methods for SQLStore

--- subastadb/ratings_sql.go
+++ subastadb/ratings_sql.go
@@ -2,24 +2,34 @@ package subastadb
 
 import (
 	"context"
+	"fmt"
 
 	orderT "github.com/lightninglabs/pool/order"
 	"github.com/lightninglabs/subasta/ratings"
+	"github.com/lightninglabs/subasta/subastadb/postgres"
 )
 
 // IndexRatings indexes the set of ratings to get the most up to date
 // state. No other calls should be executed before this one.
 func (s *SQLStore) IndexRatings(context.Context) error {
-	return ErrNotImplemented
+	// There's nothing to index so we can return immediately.
+	return nil
 }
 
 // LookupNode attempts to look up a rating for a node. If the node
 // isn't found, the lowest rating should be returned. The second return
 // value signifies if this node was found in the DB or not.
-func (s *SQLStore) LookupNode(context.Context, [33]byte) (orderT.NodeTier,
-	bool) {
+func (s *SQLStore) LookupNode(ctx context.Context,
+	nodeKey [33]byte) (orderT.NodeTier, bool) {
 
-	return 0, false
+	rating, err := s.queries.GetNodeRating(ctx, nodeKey[:])
+	if err != nil {
+		log.Errorf("unable to retrieve rating for node %x: %v", nodeKey,
+			err)
+		return orderT.NodeTier0, false
+	}
+
+	return orderT.NodeTier(rating.NodeTier), true
 }
 
 // ModifyNodeRating attempts to modify the rating for a node in-place.
@@ -28,15 +38,44 @@ func (s *SQLStore) LookupNode(context.Context, [33]byte) (orderT.NodeTier,
 // tracked.
 //
 // TODO(roasbeef): have only batched versions of this and the above?
-func (s *SQLStore) ModifyNodeRating(context.Context, [33]byte,
-	orderT.NodeTier) error {
+func (s *SQLStore) ModifyNodeRating(ctx context.Context, nodeKey [33]byte,
+	tier orderT.NodeTier) error {
 
-	return ErrNotImplemented
+	params := postgres.UpsertNodeRatingParams{
+		NodeKey:  nodeKey[:],
+		NodeTier: int64(tier),
+	}
+	return s.queries.UpsertNodeRating(ctx, params)
 }
 
 // NodeRatings returns a map of all node ratings known to the database.
 func (s *SQLStore) NodeRatings(ctx context.Context) (ratings.NodeRatingsMap,
 	error) {
 
-	return nil, ErrNotImplemented
+	var rows []postgres.NodeRating
+	txBody := func(txQueries *postgres.Queries) error {
+		var err error
+
+		params := postgres.GetNodeRatingsParams{}
+		rows, err = txQueries.GetNodeRatings(ctx, params)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err := s.ExecTx(ctx, txBody)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get node ratings: %v", err)
+	}
+
+	ratings := make(ratings.NodeRatingsMap, len(rows))
+	for _, row := range rows {
+		var nodeKey [33]byte
+		copy(nodeKey[:], row.NodeKey)
+
+		ratings[nodeKey] = orderT.NodeTier(row.NodeTier)
+	}
+
+	return ratings, nil
 }
